4592f6d2024502881007115acffcbf4ecfb6cd69
OrdoRefactorum <106595075+OrdoRefactorum@users.noreply.github.com>
2023-04-12 09:34:05 +0200

    Added initial draft of app
    to be able to further develop functionality and fix bugs.

---
+++ task-timer.js
@@ -0,0 +1,96 @@
+// Get HTML elements
+const taskNameInput = document.getElementById("task-name");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+
+let activeTaskId = null;
+
+// Function to create a new task element
+function createTaskElement(taskName) {
+    // Create elements
+    const taskContainer = document.createElement("div");
+    const taskNameSpan = document.createElement("span");
+    const taskTimerSpan = document.createElement("span");
+    const taskActionsSpan = document.createElement("span");
+    const startBtn = document.createElement("button");
+    const stopBtn = document.createElement("button");
+    const resetBtn = document.createElement("button");
+
+    // Set element attributes
+    taskContainer.classList.add("task");
+    taskNameSpan.classList.add("task-name");
+    taskNameSpan.textContent = taskName;
+    taskTimerSpan.classList.add("task-timer");
+    taskTimerSpan.textContent = "00:00:00";
+    taskActionsSpan.classList.add("task-actions");
+    startBtn.textContent = "Start";
+    startBtn.addEventListener("click", startTimer);
+    stopBtn.textContent = "Stop";
+    stopBtn.addEventListener("click", stopTimer);
+    resetBtn.textContent = "Reset";
+    resetBtn.addEventListener("click", resetTimer);
+
+    // Append elements to task container
+    taskContainer.appendChild(taskNameSpan);
+    taskContainer.appendChild(taskTimerSpan);
+    taskActionsSpan.appendChild(startBtn);
+    taskActionsSpan.appendChild(stopBtn);
+    taskActionsSpan.appendChild(resetBtn);
+    taskContainer.appendChild(taskActionsSpan);
+
+    return taskContainer;
+}
+
+// Function to add a new task to the list
+function addTask() {
+    const taskName = taskNameInput.value;
+    if (taskName === "") return;
+
+    // Create new task element
+    const taskElement = createTaskElement(taskName);
+
+    // Add task element to task list
+    taskList.appendChild(taskElement);
+
+    // Clear task name input
+    taskNameInput.value = "";
+}
+
+// Function to start the timer for a task
+function startTimer(event) {
+    const taskElement = event.target.closest(".task");
+    if (taskElement === null) return;
+    if (activeTaskId !== null) {
+        stopTimer();
+    }
+    activeTaskId = setInterval(() => {
+        const taskTimerElement = taskElement.querySelector(".task-timer");
+        const [hours, minutes, seconds] = taskTimerElement.textContent.split(":").map(parseFloat);
+        const newSeconds = seconds + 1;
+        const newMinutes = minutes + Math.floor(newSeconds / 60);
+        const newHours = hours + Math.floor(newMinutes / 60);
+        const timerText = `${newHours.toString().padStart(2, "0")}:${newMinutes.toString().padStart(2, "0")}:${(newSeconds % 60).toString().padStart(2, "0")}`;
+        taskTimerElement.textContent = timerText;
+    }, 1000);
+    taskElement.classList.add("active-task");
+}
+
+// Function to stop the timer for a task
+function stopTimer() {
+    const activeTaskElement = document.querySelector(".active-task");
+    if (activeTaskElement === null) return;
+    clearInterval(activeTaskId);
+    activeTaskId = null;
+    activeTaskElement.classList.remove("active-task");
+}
+
+// Function to reset the timer for a task
+function resetTimer(event) {
+    const taskElement = event.target.closest(".task");
+    if (taskElement === null) return;
+    const taskTimerElement = taskElement.querySelector(".task-timer");
+    taskTimerElement.textContent = "00:00:00";
+}
+
+// Event listeners
+addTaskBtn.addEventListener("click", addTask);
